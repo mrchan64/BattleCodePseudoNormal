94a13284140e2f50b96fd6fded263192922e0eca
mrchan <wildcatrobotics2011@gmail.com>
2017-02-15 14:11:04 -0800

    final test

--- BattleCodePseudoNormal/src/TestClass.java
+++ BattleCodePseudoNormal/src/TestClass.java
@@ -4,8 +4,23 @@ import java.util.Arrays;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(args));
-		
+		for(int i = -100; i<100; i++){
+			for(int j = -100; j<100; j++){
+				for(int k = -100; k<100; k++){
+					int a = i;
+					int b = j;
+					int c = k;
+					int value1 = not(a) | not(b++) & c;
+					int value2 = ((a - 1) | (b/2)) & (c*=2);
+					int value3 = (a-- | --b) & (c+=2);
+					int value4 = a | not(b & --c);
+					if(value1 == 0 && value2 == 0 && value3 == 0 && value4 == 0){
+						System.out.println(i+" "+j+" "+k);
+					}
+				}
+			}
+		}
+		Float.par
 	}
 	
 	public static void remove(int[] arg){
@@ -22,6 +37,14 @@ public class TestClass {
 			newArg[i] = arg[i+1];
 		}
 		return newArg;
+	}
+	
+	public static int not(int input){
+		if(input == 0){
+			return 1;
+		}else{
+			return 0;
+		}
 	}
 
 }
